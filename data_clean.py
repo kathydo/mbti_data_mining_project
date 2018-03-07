a8ead34c1d2970fa8ecc96a14040d4b8cf2400cc
kathydo <kdodph9@gmail.com>
2018-03-07 15:38:38 -0800

    Update data_clean.py
    break down regex into 3 separate steps for 3 pipes, urls, and numbers

--- data_clean.py
+++ data_clean.py
@@ -21,7 +21,7 @@ mbti_raw_data = pd.read_csv("/Users/kathydo/Documents/School/SENG_474/Project/mb
 #print(type(mbti_raw_data))
 N = mbti_raw_data.shape[0]
 
-print type(mbti_raw_data.iloc[0]['posts'])
+#print type(mbti_raw_data.iloc[0]['posts'])
 
 #text_urls_removed = re.sub(r'^https?:\/\/.*[\r\n]*', '', text_post)
 #https://stackoverflow.com/questions/11331982/how-to-remove-any-url-within-a-string-in-python
@@ -29,9 +29,9 @@ print type(mbti_raw_data.iloc[0]['posts'])
 for i in range(0,N):
 	text_post = mbti_raw_data.iloc[i]['posts']
 	#df2['amount'] = df2['amount'].str.replace('|||', ' ')
-	text_urls_removed = re.sub(r'(http\S+)|([0-9]+)|(\|\|\|)', ' ', text_post)
-	#text_urls_removed = re.sub(r'[0-9]+', ' ', text_urls_removed)
-	#text_urls_removed = re.sub(r'\|\|\|', ' ', text_urls_removed)
-	mbti_raw_data.iloc[i]['posts'] = text_urls_removed
+	text_3_pipe_removed = re.sub(r'\|\|\|', ' ', text_post)
+	text_urls_removed = re.sub(r'(http\S+)', ' ', text_3_pipe_removed)
+	text_numbers_removed = re.sub(r'[0-9]+', ' ', text_urls_removed)
+	mbti_raw_data.iloc[i]['posts'] = text_numbers_removed
 
-mbti_raw_data.to_csv('out.csv')
+mbti_raw_data.to_csv('mbti_data_scrub.csv')
