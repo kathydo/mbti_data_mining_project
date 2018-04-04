c939f2a3563cfeb825eaa9108fab55c1057897c1
kathydo <kdodph9@gmail.com>
2018-04-04 14:37:50 -0700

    Add files via upload

---
+++ CSV files of y data/load_y_data.py
@@ -0,0 +1,18 @@
+from numpy import genfromtxt
+
+y_train_IE = genfromtxt('y_train_IE.csv', delimiter=',')
+y_test_IE = genfromtxt('y_test_IE.csv', delimiter=',')
+y_val_IE = genfromtxt('y_val_IE.csv', delimiter=',')
+y_train_NS = genfromtxt('y_train_NS.csv', delimiter=',')
+y_test_NS = genfromtxt('y_test_NS.csv', delimiter=',')
+y_val_NS = genfromtxt('y_val_NS.csv', delimiter=',')
+y_train_TF = genfromtxt('y_train_TF.csv', delimiter=',')
+y_test_TF = genfromtxt('y_test_TF.csv', delimiter=',')
+y_val_TF = genfromtxt('y_val_TF.csv', delimiter=',')
+y_train_JP = genfromtxt('y_train_JP.csv', delimiter=',')
+y_test_JP = genfromtxt('y_train_JP.csv', delimiter=',')
+y_val_JP = genfromtxt('y_val_JP.csv', delimiter=',')
+
+#now you can work with the y vectors from here
+
+
